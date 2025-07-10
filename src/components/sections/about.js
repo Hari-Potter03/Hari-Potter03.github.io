f7f9fea5c1a79ba87034431a8bc66f482356fec4
Hari-Potter03 <srsr9160@colorado.edu>
2025-07-09 20:12:11 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,30 +135,18 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Srihari and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! I’m Srihari, a recent graduate from the University of Colorado at Boulder where
+              I majored in Statistics and Data Science.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I’m currently looking for opportunities in data science, machine learning engineering,
+              or data analyst roles. I’ve gained hands-on experience through internships at,{' '}
+              <a href="https://www.pge.com/">PG&E</a> and{' '}
+              <a href="https://quadwave.com/">Quadwave Consulting</a>, and also worked as a data
+              management specialist at,{' '}
+              <a href="https://www.colorado.edu/alumni/">CU Boulder Alumni Services</a>. I enjoy
+              using data to drive insights and build meaningful solutions.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
